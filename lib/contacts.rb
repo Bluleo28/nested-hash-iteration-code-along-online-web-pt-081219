e029b8a0e41507866207da0f9a1336c22f6e741f
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-26 03:42:12 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,42 +1,16 @@
-> person
-=> "Jon Snow"
- 
-> contact_details_hash
-=> {:name=>"Jon", :email=>"jon_snow@thewall.we", :favorite_ice_cream_flavors=>["chocolate", "vanilla"]}
-   },
-  contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
-      contact_details_hash.each do |attribute, data|
-    end
-  end
-end
-> attribute
-=> :name
- 
-> data
-=> "Jon"
-contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-      end
-    end
-  end
-contacts.each do |person, contact_details_hash|
-  if person == "Freddy Mercury"
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
-      end
-    end
-  end
-def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-      if person == "Freddy Mercury"
-        contact_details_hash.each do |attribute, data|
-          if attribute == :favorite_ice_cream_flavors
-            data.delete_if {|ice_cream| ice_cream == "strawberry"}
-        end
-      end
-    end
+require_relative "../lib/contacts.rb"
 
+contacts = {
+   "Jon Snow" => {
+     name: "Jon",
+     email: "jon_snow@thewall.we", 
+     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+   },
+   "Freddy Mercury" => {
+     name: "Freddy",
+     email: "freddy@mercury.com",
+     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+   }
+ }
+
+puts remove_strawberry(contacts)
