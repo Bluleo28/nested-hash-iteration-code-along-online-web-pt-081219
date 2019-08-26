c6bc9e94999e6c19d6458a3e90a50fce8384d5a4
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-26 03:54:56 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,10 +4,10 @@ def contact_details_hash
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
            data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
-end
 end   
 
 #   contacts = {
