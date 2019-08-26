37dc22674442ae97f7b273f5c380d1c6c6adf2fc
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-26 03:51:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -2,7 +2,6 @@ def contact_details_hash
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
-      binding.pry
     end
   end
 end
