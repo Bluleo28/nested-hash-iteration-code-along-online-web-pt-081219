33f59aefa38a10588c376e1c6c79284878052694
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-26 03:33:51 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,4 @@
-rcontacts.each do |person, contact_details_hash|
-end
-  > person
+> person
 => "Jon Snow"
  
 > contact_details_hash
@@ -24,7 +22,6 @@ contacts.each do |person, contact_details_hash|
       end
     end
   end
-end
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
@@ -33,7 +30,6 @@ contacts.each do |person, contact_details_hash|
       end
     end
   end
-end
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
@@ -43,5 +39,4 @@ def remove_strawberry(contacts)
         end
       end
     end
-  end
-end
+
